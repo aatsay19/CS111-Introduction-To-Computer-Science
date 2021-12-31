544e9dd0765e6db806fd0f179bdf9305acd17395
Aatif Sayed <49285804+aatsay19@users.noreply.github.com>
2021-12-30 21:57:02 -0500

    Added HelloWorld.java program

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author: Aatif Sayed
+ *	NetID:   as3259
+ *	E-mail:  as3259@rutgers.edu
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+
+    }
+
+}
